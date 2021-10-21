229a943df2bd7aa8d78b05bc88b861976ea0b66c
Dominik Snopek <222572@edu.p.lodz.pl>
2021-10-21 13:20:59 +0200

    use StringInterpreter to analise given string and return result

--- src/main/java/com/example/zad2ppkwu/Controller/StringController.java
+++ src/main/java/com/example/zad2ppkwu/Controller/StringController.java
@@ -1,5 +1,6 @@
 package com.example.zad2ppkwu.Controller;
 
+import com.example.zad2ppkwu.StringInterpreter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class StringController {
 
     @GetMapping("/{text}")
-    public String analizeString(@PathVariable String text){
-        return null;
+    public StringInterpreter analizeString(@PathVariable String text){
+
+        StringInterpreter stringInterpreter = new StringInterpreter();
+        stringInterpreter.analise(text);
+
+        return stringInterpreter;
     }
 
 }

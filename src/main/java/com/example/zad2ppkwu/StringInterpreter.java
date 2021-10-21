c7f7c2fc663b3ce119e13484a2fd4cbd5dd41aa6
Dominik Snopek <222572@edu.p.lodz.pl>
2021-10-21 13:19:46 +0200

    create method analise in StringInterpreter.java to analise given string

--- src/main/java/com/example/zad2ppkwu/StringInterpreter.java
+++ src/main/java/com/example/zad2ppkwu/StringInterpreter.java
@@ -10,7 +10,26 @@ public class StringInterpreter {
     private Integer number;
     private Integer specialChar;
 
-    
+    public void analise(String text) {
+        givenString = text;
+        length = text.length();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (Character.isDigit(text.charAt(i))) {
+                number++;
+            }else if (Character.isLetter(text.charAt(i))) {
+                letter++;
+                if(Character.isLowerCase(text.charAt(i))){
+                    lowerCase++;
+                }else{
+                    upperCase++;
+                }
+            }else{
+                specialChar++;
+            }
+        }
+
+    }
 
 
 }

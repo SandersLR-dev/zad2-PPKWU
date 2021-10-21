64056ac04b0d910e793ffc03869ba905b9ecee52
Dominik Snopek <222572@edu.p.lodz.pl>
2021-10-21 13:18:37 +0200

    create class StringInterpreter.java to a save analize

---
+++ src/main/java/com/example/zad2ppkwu/StringInterpreter.java
@@ -0,0 +1,16 @@
+package com.example.zad2ppkwu;
+
+public class StringInterpreter {
+
+    private String givenString;
+    private Integer length;
+    private Integer letter;
+    private Integer lowerCase;
+    private Integer upperCase;
+    private Integer number;
+    private Integer specialChar;
+
+    
+
+
+}
